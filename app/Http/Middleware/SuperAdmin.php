2168ce8a292db7436be3df9af7b7cfdd1d3f3283
relentlesstrout <relentlesstrout@gmail.com>
2026-02-12 17:18:13 +0000

    Added super admin scope and reduced manage accounts to super admins

---
+++ app/Http/Middleware/SuperAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SuperAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        if (! auth()->check() || ! auth()->user()->isSuperAdmin()) {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+
+}
